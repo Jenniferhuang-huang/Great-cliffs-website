121c2ebaa27266c5692ac2c888fe960ddf45e8e3
Jiahui Huang <jiahuihuang@Jiahuis-MacBook-Pro.local>
2022-01-31 16:15:09 -0500

    modify disablebutton function

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -2,6 +2,8 @@
 function generateCode(){
     //Creat variables to store generated codes and the type of the charaters we want to show as codes
     var code = ' '; //initialize to null value
+    var fetCode = ' ';
+    var btnvalue;
     var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$'
     for (i=1; i <= 8; i++){
         var char = Math.random()*str.length;
@@ -11,7 +13,17 @@ function generateCode(){
 }
 document.getElementById("codes").innerHTML=generateCode();
 
-function disableButton(){
-    document.getElementById("submit").disabled = true;
+function disableButton(btnvalue){
+    document.getElementById("submit").disabled = btnvalue;
+    if (btnvalue == true){
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119,209, 0.3)";
+        document.getElementById("submit").style.Color = "rgba(225, 225,225, 0.5)";
+    } else {
+        document.getElementById("submit").style.backgroundColor =
+          "rgba(73, 119,209, 1)";
+        document.getElementById("submit").style.Color =
+          "rgba(225, 225,225, 1)";
+    }
 } 
+
 disableButton();
