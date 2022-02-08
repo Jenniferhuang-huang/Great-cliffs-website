cf761e992ecf36edeaa439a7844c0eb94a337f72
Jiahui Huang <jiahuihuang@Jiahuis-MacBook-Pro.local>
2022-02-08 01:20:20 -0500

    display box JS function and CSS styling

--- js/activities.js
+++ js/activities.js
@@ -3,7 +3,24 @@ $(document).ready(function () {
     var content = $(this).text();
     if (content != "Not Available") {
       $(this).css("cursor", "pointer");
-      $(this).toggleClass("highlight");
+      $(this).toggleClass("tdhighlight");
+
+      if ($(this).hasClass("tdhighlight")) {
+        $("#displaySelected").css({
+          visibility: "visible",
+          "margin-top": "2em",
+        });
+        $("#result").append("<p>" + content + "</p>");
+      } else {
+        $("#result p:contains(" + content + ")").remove();
       }
-    })
-  })
+      
+      if ($("#result").has("p").length == false) {
+        $("#displaySelected").css({
+          visibility: "hidden",
+          "margin-top": "0",
+        });
+      }
+    }
+  });
+});
